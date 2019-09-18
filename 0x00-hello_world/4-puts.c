d8c22489735afd478a5d2c05db1893e7056f5931
Cris-Mur <yomy98@gmail.com>
2019-09-18 17:32:34 -0500

    [ADD FILE]

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - entry point
+*
+* return: 0 (success)
+*/
+int main(void)
+{
+	char phrase[] = "Programming is like building a multilingual puzzle";
+
+	puts(phrase);
+	return (0);
+}
