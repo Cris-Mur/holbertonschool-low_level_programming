42192538ec28d4067eeee7c04ff804af419e09a5
Cris-Mur <yomy98@gmail.com>
2019-09-18 17:49:37 -0500

    [ADD FILE]4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
-* main - entry point
-*
-* return: 0 (success)
-*/
+ * main - entry point
+ *
+ * Return: always 0 (Success)
+ */
 int main(void)
 {
 	char phrase[] = "Programming is like building a multilingual puzzle";
