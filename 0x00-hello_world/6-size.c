f4ca7f77a4aa9bf542123898a902b7e698a945c2
Cris-Mur <yomy98@gmail.com>
2019-09-18 19:49:26 -0500

    [FIX] change documentation (return: ... ) to (Return: ...)

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,16 +3,15 @@
 /**
  * main - entry point
  *
- * return: always 0 (success)
+ * Return: always 0 (success)
  *
  */
 int main(void)
 {
-	printf("Size of a char: %lu byte(s)\n", sizeof(char));
-	printf("Size of an int: %lu byte(s)\n", sizeof(int));
-	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
-	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long));
-	printf("Size of a float: %lu byte(s)\n", sizeof(float));
-	
+	printf("Size of a char: %li byte(s)\n", sizeof(char));
+	printf("Size of an int: %li byte(s)\n", sizeof(int));
+	printf("Size of a long int: %li byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %li byte(s)\n", sizeof(long long));
+	printf("Size of a float: %li byte(s)\n", sizeof(float));
 	return (0);
 }
