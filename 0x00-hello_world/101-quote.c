43de0b06f3f277ae93504df810834e0464f77390
Cris-Mur <yomy98@gmail.com>
2019-09-18 21:37:27 -0500

    [FIX]DOCUMENTATION

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 /**
  * main - entry point
  *
- * Return: always 0 (Success)
+ * Return: always 1 (ERROR)
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
