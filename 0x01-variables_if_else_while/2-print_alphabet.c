2cb8dd2c77e0491f084590cd80ea0db4b226a187
Cris-Mur <yomy98@gmail.com>
2019-09-19 13:09:52 -0500

    [ADD FILE]

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	char  c;
+	for (c = 97; c <= 122; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
