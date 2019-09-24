0f89d0adedea696f9eefabc1e77b637546d498f8
Cris-Mur <yomy98@gmail.com>
2019-09-24 11:52:07 -0500

    [fix]remplace array to putchar funtion

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -7,12 +7,15 @@
  */
 int main(void)
 {
-	char str[9] = "Holberton";
-
-	for (int i = 0; i <= 9; i++)
-	{
-		_putchar(str[i]);
-	}
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
 	_putchar('\n');
 	return (0);
 }
