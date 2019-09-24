6c02462bf18f4fa83cc69166b193fc67ce2458fd
Cris-Mur <yomy98@gmail.com>
2019-09-24 16:03:01 -0500

    [ wanna cry ]

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,37 +6,41 @@
  * @n: parameter input
  * Return: void function
  */
- void print_to_98(int n)
- {
-	 while (n < 98)
-	 {
-		 printf("%d", n);
+void print_to_98(int n)
+{
+	while (n < 98)
+	{
+		printf("%d", n);
 
-		 if (n == 98)
-		 {
-			 printf("\n");
-		 }
-		 else
-		 {
-			 printf(", ");
-		 }
-		 n++;
-	 }
-	 while (n > 98)
-	 {
-		 printf("%d", n);
-		 if (n == 98)
-		 {
-			 printf("\n");
-		 }
-		 else
-		 {
-			 printf(", ");
-		 }
-		 n--;
-	 }
-	 if (n == 98)
-	 {
-		 printf("%d\n", n);
-	 }
- }
+		if (n == 98)
+		{
+			printf("\n");
+		}
+		else
+		{
+			printf(", ");
+		}
+		n++;
+	}
+
+	while (n > 98)
+
+	{
+		printf("%d", n);
+
+		if (n == 98)
+		{
+			printf("\n");
+		}
+		else
+		{
+			printf(", ");
+		}
+		n--;
+	}
+
+	if (n == 98)
+	{
+		printf("%d\n", n);
+	}
+}
