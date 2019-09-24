24669e155c67fa46d30858b4ff81877a174d1a62
Cris-Mur <yomy98@gmail.com>
2019-09-24 18:22:38 -0500

    [iluminación divina]

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -11,39 +11,17 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && a > c)
+	if (a >= b && a >= c)
 	{
 		largest = a;
 	}
-	else if (b > a && b > c)
+	else if (b >= a && b >= c)
 	{
 		largest = b;
 	}
-	else if (c > a && c > b)
+	else if (c >= a && c >= b)
 	{
 		largest = c;
-	}
-	else if (a == b)
-	{
-		if (a > c)
-		{
-			largest = a;
-		}
-		else
-		{
-			largest = c;
-		}
-	}
-	else if (b == c)
-	{
-		if (b > a)
-		{
-			largest = b;
-		}
-		else
-		{
-			largest = a;
-		}
 	}
 
 	return (largest);
