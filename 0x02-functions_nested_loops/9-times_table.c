c551576f7857eed31fe079a6d74d6165a8e3f133
Cris-Mur <yomy98@gmail.com>
2019-09-24 15:32:36 -0500

    [fix] betty's and compiler recomendations

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,19 +8,22 @@
 
 void times_table(void)
 {
+	int colum, fila;
+
 	for (int colum = 0; colum <= 9; colum++)
 	{
 		for (int fila = 0; fila <= 9; fila++)
 		{
 			int result = colum * fila;
+
 			if (result <= 9)
 			{
 				_putchar(result + 48);
 			}
 			else if (result >= 10)
 			{
-				_putchar((result/10) + 48);
-				_putchar((result%10) + 48);
+				_putchar((result / 10) + 48);
+				_putchar((result % 10) + 48);
 			}
 			if (fila < 9 && result < 10)
 			{
