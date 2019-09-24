ef93c13bf3e70c419c31081f55826ebf01ac2b4f
Cris-Mur <yomy98@gmail.com>
2019-09-24 15:53:44 -0500

    [yes sr]

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,11 +2,12 @@
 
 /**
  * add - adds two integers and returns the result
- *
+ * @a: parameters input
+ * @b: parameters input
  * Return: adds twu integers
  */
 
 int add(int a, int b)
 {
-	return (a+b);
+	return (a + b);
 }
