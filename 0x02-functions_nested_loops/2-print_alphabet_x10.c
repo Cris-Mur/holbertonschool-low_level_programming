b0fdc94fb8203ccf279662c76d6b9034e9b7d8e9
Cris-Mur <yomy98@gmail.com>
2019-09-24 12:07:12 -0500

    [fix]change loop for by create symbols to loop for array

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,11 +7,13 @@
  */
 void print_alphabet_x10(void)
 {
-	for (int i = 0; i <= 10; i++)
+	char c[26] = "abcdefghijklmnopqrstuvwxyz";
+
+	for (int j = 0; j <= 10; j++)
 	{
-		for (char c = 97; c <= 122; c++)
+		for (int i = 0; i <= 26; i++)
 		{
-			_putchar(c);
+			_putchar(c[i]);
 		}
 		_putchar('\n');
 	}
