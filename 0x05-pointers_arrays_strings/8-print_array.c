c1ce7f8ab72d0ccba7586706a494762ba3051bcf
Cris-Mur <yomy98@gmail.com>
2019-10-01 16:15:24 -0500

    [add files] modify holberton.h

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_array
+ * @a: input array
+ * @n: input size of array
+ * Return: void
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	i = 0;
+	while (n > 0)
+	{
+		printf("%d", a[i]);
+		i++;
+		n--;
+		if (n > 0)
+		{
+			printf(", ");
+		}
+		else
+		{
+			printf("\n");
+		}
+	}
+}
