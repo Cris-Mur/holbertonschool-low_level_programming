5bb2f3dfca06a5cb1a42bcf69db34f26f6b316e3
Cris-Mur <yomy98@gmail.com>
2019-10-01 18:08:41 -0500

    [the life is bad]

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,18 +12,25 @@ void print_array(int *a, int n)
 	int i;
 
 	i = 0;
-	while (n > 0)
+	if (n > 0)
 	{
-		printf("%d", a[i]);
-		i++;
-		n--;
-		if (n > 0)
+		while (n > 0)
 		{
-			printf(", ");
+			printf("%d", a[i]);
+			i++;
+			n--;
+			if (n > 0)
+			{
+				printf(", ");
+			}
+			else
+			{
+				printf("\n");
+			}
 		}
-		else
-		{
-			printf("\n");
-		}
+	}
+	else
+	{
+		printf("\n");
 	}
 }
