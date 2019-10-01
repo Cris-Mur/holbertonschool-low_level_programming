0ad7c9a11d46a3da9c950ee0e145d44c211cc029
Cris-Mur <yomy98@gmail.com>
2019-10-01 15:47:42 -0500

    [modify & add files]

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+ * puts_half - prins half string
+ * @str: input pointer
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int n, ci;
+
+	while (str[n] != 00)
+	{
+		n++;
+		ci = n;
+	}
+	if (n % 2 != 0 )
+	{
+		n = (n - 1) / 2;
+	}
+	else
+	{
+		n = n/2;
+	}
+	while (n < ci)
+	{
+		_putchar(str[n]);
+		n++;
+	}
+	_putchar(10);
+}
