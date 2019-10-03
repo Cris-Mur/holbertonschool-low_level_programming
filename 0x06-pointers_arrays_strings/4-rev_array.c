92029a58b007e658a985c6fd396e884a08b9905d
Cris-Mur <yomy98@gmail.com>
2019-10-03 17:32:54 -0500

    [add files]

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * reverse_array - reverse array
+ * @a: array input
+ * @n: elements of array
+ * return: void
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i, aux;
+
+	i = 0;
+	while (n > i)
+	{
+		aux = a[i];
+		a[i] = a[n];
+		a[n] = aux;
+		n--;
+		i++;
+	}
+}
