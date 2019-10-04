c17aba81ec894ae2ac010cef4cb678c17c131f74
Cris-Mur <yomy98@gmail.com>
2019-10-03 23:18:44 -0500

    [add files]

---
+++ 0x06-pointers_arrays_strings/103-print_buffer.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * print_buffer - print a buffer
+ * @b: input pointer
+ * @size: input parameter
+ * Return: void
+ */
+
+void print_buffer(char *b, int size)
+{
+	size++;
+}
