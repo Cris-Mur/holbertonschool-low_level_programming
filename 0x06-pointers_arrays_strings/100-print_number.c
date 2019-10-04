8c19b7e1ac2b93d76d50d55c1ede68e94560b51a
Cris-Mur <yomy98@gmail.com>
2019-10-03 23:13:53 -0500

    [add files]

---
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * print_number - print number
+ * @n: input integer
+ * Return: void
+ */
+
+void print_number(int n)
+{
+	n++;
+}
---
+++ 0x06-pointers_arrays_strings/102-infinite_add.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * infinite_add - add two numbers
+ * @n1: pointer input
+ * @n2:
+ * @r:
+ * @size_r:
+ * Return: r (success) or 0 (error)
+ */
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
