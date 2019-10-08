0da3b5ff6ee836cb5457d1f886886ada6e7b8189
Cris-Mur <yomy98@gmail.com>
2019-10-07 19:30:04 -0500

    [add files]

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * _strpbrk - search in a string
+ * @s: string input
+ * @accept: string matches
+ * Return: string match
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+
+	for (i = 0; s[i] != 00; i++)
+	{
+		for (j = 0; accept[j] != 00; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				return (s + 1);
+			}
+		}
+	}
+	return (00);
+}
