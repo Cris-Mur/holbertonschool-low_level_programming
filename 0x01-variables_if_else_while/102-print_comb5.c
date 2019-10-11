25e1f6e0e6663b174e77eb8b682a7aaa359c6676
Cris-Mur <yomy98@gmail.com>
2019-10-11 01:17:53 +0000

    [refactured]

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - entry point
  *
@@ -7,38 +6,27 @@
  */
 int main(void)
 {
-	int n1, n2, n3, n4;
+	int n1, n2;
 
-	for (n1 = 48; n1 <= 57; n1++)
+	for (n1 = 0; n1 <= 99; n1++)
 	{
-		for (n2 = 48; n2 <= 57; n2++)
+		for (n2 = 0; n2 <= 99; n2++)
 		{
-			for (n3 = 48; n3 <= 57; n3++)
+			if (n1 < n2)
 			{
-				for (n4 = 48; n4 <= 57; n4++)
+				putchar((n1 / 10) + 48);
+				putchar((n1 % 10) + 48);
+				putchar(32);
+				putchar((n2 / 10) + 48);
+				putchar((n2 % 10) + 48);
+				if (n1 != 98 || n2 != 99)
 				{
-					int sum1, sum2;
-
-					sum1 = ((n1 - 48) * 10) + (n2 - 48);
-					sum2 = ((n3 - 48) * 10) + (n4 - 48);
-
-					if (sum1 < sum2)
-					{
-						putchar(n1);
-						putchar(n2);
-						putchar(32);
-						putchar(n3);
-						putchar(n4);
-						if (sum1 != 113 || sum2 != 114)
-						{
-							putchar(44);
-							putchar(32);
-						}
-					}
+					putchar(44);
+					putchar(32);
 				}
 			}
 		}
 	}
-	putchar('\n');
-	return (0);
+putchar('\n');
+return (0);
 }
