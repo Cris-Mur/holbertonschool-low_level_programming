dd3f827685de84ea331e5cb901ec86becb51803f
Cris-Mur <yomy98@gmail.com>
2019-10-15 20:00:43 +0000

    [add task 0]

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * create_array - creation and inicialize array
+ * @size: size of array
+ * @c: input character
+ * Return: new array
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *array;
+
+	i = 0;
+	if (size != 0)
+	{
+		array = malloc(size);
+		while (i < size)
+		{
+			array[i] = c;
+			i++;
+		}
+	}
+	else
+	{
+		array = 00;
+	}
+	return (array);
+}
