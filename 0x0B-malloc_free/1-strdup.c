acbe46c72e9e04ed087fe5d4ed860ed84f570d93
Cris-Mur <yomy98@gmail.com>
2019-10-15 20:42:11 +0000

    [add files] task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - put input string in other string
+ * @str: input string
+ * Return: string
+ */
+
+char *_strdup(char *str)
+{
+	int size, i;
+	char *new;
+
+	if (str != 00)
+	{
+		size = 0;
+		while (str[size] != 00)
+		{
+			size++;
+		}
+		size++;
+
+		new = malloc(size);
+		for (i = 0; i <= size; i++)
+		{
+			new[i] = str [i];
+		}
+	}
+	else
+	{
+		return (str);
+	}
+	return (new);
+}
