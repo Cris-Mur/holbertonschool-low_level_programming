6ad3b76e71f644ed72df6b8e894801911905d1f1
Cris-Mur <yomy98@gmail.com>
2019-10-16 06:24:37 +0000

    [add task and finish]

--- 0x0B-malloc_free/5-argstostr.c
+++ 0x0B-malloc_free/5-argstostr.c
@@ -2,10 +2,32 @@
 
 /**
  * argstostr -
- *
- * Return: 
+ * @ac: argument counter
+ * @av: argument vector
+ * Return: av string
  */
 char *argstostr(int ac, char **av)
 {
+	int i, a, j, k;
+	char *new;
 
+	a = ac;
+
+	for (i = 0; av[i] && a > 0; i++)
+	{
+		for (j = 0; av[i][j] && a > 0; j++)
+		{
+			if (av[i][j + 1] == 00)
+			{
+				j++;
+				av[i][j] = 10;
+				a--;
+			}
+		}
+	}
+
+	new = malloc(sizeof(char *) * j);
+	for (k = 0; k < j; k++)
+		new[k] = av[0][k];
+	return (new);
 }
