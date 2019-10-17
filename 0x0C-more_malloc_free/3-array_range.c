a655a4e86caa01616828f2d31cdeb14e1ba3da73
Cris-Mur <yomy98@gmail.com>
2019-10-17 23:02:10 +0000

    [señor de los cielos]

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,11 +1,30 @@
 #include "holberton.h"
 
 /**
- * array_range -
- *
- * Return: 
+ * array_range - array range
+ * @min: value minimum
+ * @max: value max
+ * Return: pointer
  */
 int *array_range(int min, int max)
 {
-
+	int *arr, sum, i;
+	if (min > max)
+	{
+		return (NULL);
+	}
+	else
+	{
+		sum = max - min;
+		arr = malloc(sizeof(int) * (sum + 1));
+		if (!arr)
+		{
+			return (NULL);
+		}
+		for (i = 0; min <= max; i++, min++)
+		{
+			arr[i] = min;
+		}
+	}
+	return (arr);
 }
