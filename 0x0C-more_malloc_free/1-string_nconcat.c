ba6c6c2de9f8d354dc3d2ee4514461b6c7b2a575
Cris-Mur <yomy98@gmail.com>
2019-10-17 22:51:31 +0000

    [fix Betty's]

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,23 +13,18 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *new;
 
 	if (!s1)
-	{
 		s1 = "";
-	}
 	if (!s2)
-	{
 		s2 = "";
-	}
 
 	for (size1 = 0; s1[size1]; size1++)
 	{
 	}
-
 	for (size2 = 0; s2[size2]; size2++)
 	{
 	}
 
-	if(size2 < n)
+	if (size2 < n)
 	{
 		h = size2;
 	}
