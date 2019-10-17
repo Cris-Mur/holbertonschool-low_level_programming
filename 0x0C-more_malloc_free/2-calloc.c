44c0ccf2040b653bd519f7183f94d1f0c83e75a0
Cris-Mur <yomy98@gmail.com>
2019-10-17 21:32:54 +0000

    [add task 2]

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,11 +1,28 @@
 #include "holberton.h"
 
 /**
- * _calloc -
- *
- * Return: 
+ * _calloc - my calloc
+ * @nmemb: number of memory of bytes
+ * @size: size of bytes
+ * Return: point inizialize in 0
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+	void *p;
+	unsigned int i;
 
+	if (nmemb > 0 && size)
+	{
+		P = malloc(nmemb * size);
+		if (!p)
+		{
+			free(p);
+			return(NULL);
+		}
+	}
+	for (i = 0; i < (nmemb * size); i++)
+	{
+		p[i] = 0;
+	}
+	return (p);
 }
