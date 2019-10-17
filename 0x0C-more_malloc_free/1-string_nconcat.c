5d0ecd93b5ee574c0587b6aaaa7699c3dba9f689
Cris-Mur <yomy98@gmail.com>
2019-10-17 20:51:12 +0000

    [fix assing memory space]

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -16,7 +16,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 	}
 
-	new = malloc(size + (n + 1) * sizeof(char *));
+	new = malloc(size + (n + 1) * sizeof(char));
 	if (new == NULL)
 	{
 		free(new);
