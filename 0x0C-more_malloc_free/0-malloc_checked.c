7c33775e7993116d6c74a02b76a7c0d697c8eae3
Cris-Mur <yomy98@gmail.com>
2019-10-17 17:30:03 +0000

    [add task 0]

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,11 +1,18 @@
 #include "holberton.h"
 
 /**
- * malloc_checked -
- *
- * Return: 
+ * malloc_checked - check malloc
+ * @b: is a input of malloc
+ * Return: always * (success)
  */
 void *malloc_checked(unsigned int b)
 {
-
+	void *s;
+	s = malloc(b);
+	if (s == NULL)
+	{
+		free(s);
+		exit (98);
+	}
+	return (s);
 }
