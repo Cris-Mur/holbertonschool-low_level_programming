2f397bb2c9e01616eb13b4a2ea0ee540ba60e270
Cris-Mur <yomy98@gmail.com>
2019-10-20 04:06:46 -0500

    [add task 9]

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * set_string - funtion that setting a pointer
+ * @s: input
+ * @to: string that change
+ * Return: void funtion
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
