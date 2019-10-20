89f89061cd8a50f55c836f0114b434969e667af1
Cris-Mur <yomy98@gmail.com>
2019-10-20 11:56:19 -0500

    [fix when one string is null]

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,5 +1,6 @@
 #include "holberton.h"
 
+int concatlen(char *, char*);
 /**
  * str_concat - concatenate two strings
  * @s1: input string
@@ -9,31 +10,30 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	int size1, size2, i, j;
+	int size, i, j;
 	char *new;
 
-	size1 = 0;
-	size2 = 0;
-	while (s1[size1] != 00)
+	if (!s1)
 	{
-		size1++;
+		s1 = "";
 	}
-	while (s2[size2] != 00)
+	if (!s2)
 	{
-		size2++;
+		s2 = "";
 	}
-	new = malloc(size1 + (size2 + 1) * sizeof(char));
+	size = concatlen(s1, s2);
+	new = malloc(size * sizeof(char));
 	if (new == NULL)
 	{
 		return (NULL);
 	}
-	if (size1 != 0 || size2 != 0)
+	if (size != 0)
 	{
-		for (i = 0; i < size1; i++)
+		for (i = 0; s1[i]; i++)
 		{
 			new[i] = s1[i];
 		}
-		for (j = 0; j <= size2; j++)
+		for (j = 0; s2[j]; j++)
 		{
 			new[i + j] = s2[j];
 		}
@@ -43,4 +43,15 @@ char *str_concat(char *s1, char *s2)
 		new = "";
 	}
 	return (new);
+}
+
+int concatlen(char *s1, char *s2)
+{
+	int size1, size2;
+
+	for (size1 = 0; s1[size1]; size1++)
+	{}
+	for (size2 = 0; s2[size2]; size2++)
+	{}
+	return (size1 + size2 + 1);
 }
