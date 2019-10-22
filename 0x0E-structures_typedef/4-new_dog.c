e8b77f8b5300b354427af4e89d05b9b497fd977a
Cris-Mur <yomy98@gmail.com>
2019-10-21 20:45:40 -0500

    [fix Betty's recomendations]

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -46,10 +46,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 void free_dog(dog_t *d)
 {
-        if (d)
-        {
-                free(d->name);
-                free(d->owner);
-                free(d);
-        }
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
 }
