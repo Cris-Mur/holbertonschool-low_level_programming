0fbf0807176e259aeefc49ad90d3e30be50a9ac2
Cris-Mur <yomy98@gmail.com>
2019-10-23 17:40:43 -0500

    [ADD TASK OF CALCULATOR]

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,30 @@
+#include "function_pointers.h"
+#include "3-calc.h"
+
+/**
+ * get_op_func - select which funtion will use
+ * @s: input string
+ * @a: input number
+ * @b: input number
+ * Return: pointer to funtion
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+
+	i = 0;
+	while (ops[i].op[0] != s[0] && i < 5)
+	{
+		i++;
+	}
+	return (ops[i].f);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include "function_pointers.h"
+
+/**
+ * op_add - funtion that add two numbers
+ *
+ * Return: result
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - funtion that subtract two numbers
+ *
+ * Return: result
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - funtion that multiply two numbers
+ *
+ * Return: result
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - funtion that divide two numbers
+ *
+ * Return: result
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - funtion that module two numbers
+ *
+ * Return: result
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
