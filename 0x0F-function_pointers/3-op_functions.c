9cb5beb7eee8e2e30e2e93e80c5dacc72fb968ec
Cris-Mur <yomy98@gmail.com>
2019-10-23 18:22:02 -0500

    [fix betty]

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -44,7 +44,7 @@ int op_div(int a, int b)
 	if (b == 0)
 	{
 		printf("Error\n");
-		exit (100);
+		exit(100);
 	}
 	else
 		return (a / b);
@@ -61,7 +61,7 @@ int op_mod(int a, int b)
 	if (b == 0)
 	{
 		printf("Error\n");
-		exit (100);
+		exit(100);
 	}
 	else
 		return (a % b);
