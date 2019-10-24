35fcbbab84bce569f2f1a756903ea3b702894836
Cris-Mur <yomy98@gmail.com>
2019-10-23 19:07:05 -0500

    [fix wrong inputs]

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -15,8 +15,17 @@ int main(int argc, char *argv[])
 
 		fun = get_op_func(argv[2]);
 
+
 		if (fun != 00 || strlen(argv[2]) == 1)
 		{
+			if ((argv[2][0] != 43 && argv[2][0] != 45 &&
+			    argv[2][0] != 42 && argv[2][0] != 47 &&
+			    argv[2][0] != 37) || strlen(argv[2]) != 1)
+			{
+				printf("Error\n");
+				exit(99);
+			}
+
 			printf("%d\n", fun(atoi(argv[1]), atoi(argv[3])));
 			return (0);
 		}
