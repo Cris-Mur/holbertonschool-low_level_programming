b03ee8fc1ea927d789f2f59a0ffd3bfc243e4cd2
Cris-Mur <yomy98@gmail.com>
2019-11-05 22:14:50 -0500

    [fix betty's recomends]

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -25,7 +25,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		while (temp != NULL && papu < index - 1)
 		{
 			temp = temp->next;
-			papu++;;
+			papu++;
 		}
 		coso = temp->next;
 		temp->next = temp->next->next;
