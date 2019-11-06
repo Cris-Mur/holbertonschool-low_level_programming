171ff067ca3036e61ad0bd7dcd3290a1cd5e503f
Cris-Mur <yomy98@gmail.com>
2019-11-05 21:58:32 -0500

    [fix conditional]

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -9,32 +9,31 @@
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	listint_t *temp = *head;
+
 	listint_t *coso = NULL;
 
-	if (*head != NULL)
-	{
-		if (index > 0)
-		{
-			while (temp != NULL && index > 0)
-			{
-				temp = temp->next;
-				index--;
-			}
-			coso = temp->next;
-			temp->next = temp->next->next;
-			free(coso);
-		}
-		else if (index == 0)
-		{
-			coso = temp;
-			temp = temp->next;
-			*head = temp;
-			free(coso);
-		}
-	}
-	else
+	if (*head == NULL)
 	{
 		return (-1);
+	}
+
+	if (index > 0)
+	{
+		while (temp != NULL && index > 0)
+		{
+			temp = temp->next;
+			index--;
+		}
+		coso = temp->next;
+		temp->next = temp->next->next;
+		free(coso);
+	}
+	else if (index == 0)
+	{
+		coso = temp;
+		temp = temp->next;
+		*head = temp;
+		free(coso);
 	}
 	return (1);
 }
