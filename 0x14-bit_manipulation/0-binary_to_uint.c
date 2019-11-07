c54189e3d0ccdfd12fb58ccd7eeb78c51ed0181f
Cris-Mur <yomy98@gmail.com>
2019-11-07 15:22:34 -0500

    [first commit and add task 0]

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+ * binary_to_uint - convert binary to Unsigned int
+ *
+ * Return: unsigned int
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int size, base;
+	unsigned int num;
+
+	num = 0;
+	for (size = 0; b[size]; size++)
+	{
+		if (b[size] != 48 && b[size] != 49)
+		{
+			return (0);
+		}
+	}
+	size--;
+	for (base = 1;b[size];size--)
+	{
+		if ((b[size] - 48) == 1)
+		{
+			num = num + base;
+		}
+		else
+			num = num + 0;
+		base = base << 1;
+	}
+	return (num);
+}
