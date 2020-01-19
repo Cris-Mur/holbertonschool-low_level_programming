e21df8501240456ce1ef3a2582cd272e965dceae
Cris-Mur <yomy98@gmail.com>
2020-01-19 20:59:29 +0000

    [updated task]

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -8,26 +8,24 @@
 void hash_table_print(const hash_table_t *ht)
 {
 	hash_node_t *row = NULL;
-	char *list = NULL;
+	char *nodito = NULL;
 	size_t idx = 0;
+	int first = 0;
 
 	if (ht)
 	{
 		printf("{");
 		while (idx <= ht->size)
 		{
-			if (row &&
-			    idx < ht->size &&
-			      idx != 0)
-			{
-				printf(", ");
-			}
 			row = ht->array[idx];
 			if (row)
 			{
-				list = hash_table_get(ht, row->key);
+				if (first)
+					printf(", ");
+				nodito = hash_table_get(ht, row->key);
 				printf("\'%s\': \'%s\'",
-				       ht->array[idx]->key, list);
+				       ht->array[idx]->key, nodito);
+				first = 1;
 			}
 			idx++;
 		}
