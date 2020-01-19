78021a2da987192b60580cca7263b1b41b59c74a
Cris-Mur <yomy98@gmail.com>
2020-01-19 18:48:55 +0000

    [no tested]

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,21 +11,22 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *tablita = NULL;
 	unsigned long int index = 0;
 
-	if (size > 0)
+	if (size == 0)
+		return (NULL);
+
+	tablita = malloc(sizeof(hash_table_t));
+	if (!tablita)
+		return (NULL);
+
+	tablita->array = malloc(size * sizeof(hash_node_t *));
+	if (!tablita->array)
 	{
-		tablita = malloc(sizeof(hash_table_t));
-		if (!tablita)
-			return (NULL);
+		free(tablita);
+		return (NULL);
+	}
+	for (index = 0; index < size; index++)
+		tablita->array[index] = NULL;
 
-		tablita->array = malloc(size * sizeof(hash_node_t *));
-		if (!tablita->array)
-		{
-			free(tablita);
-			return (NULL);
-		}
-		for (index = 0; index < size; index++)
-			tablita->array[index] = NULL;
-
-		tablita->size = size;
-		return (tablita);
+	tablita->size = size;
+	return (tablita);
 }
