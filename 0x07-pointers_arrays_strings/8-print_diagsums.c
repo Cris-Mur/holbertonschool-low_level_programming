f65b6f763de34a20fee822dc8606917b413e7ce1
Cris-Mur <yomy98@gmail.com>
2020-02-23 20:58:39 -0500

    [betty after long long time]

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -13,13 +13,13 @@ void print_diagsums(int *a, int size)
 
 	for (y = 0, i = 0; i < size; i++)
 	{
-		sum1 += *(a+y);
+		sum1 += *(a + y);
 		y += size + 1;
 	}
 	printf("%d, ", sum1);
-	for (y = 0, i = size-1; y < size; y++)
+	for (y = 0, i = size - 1; y < size; y++)
 	{
-		sum2 += *(a+i);
+		sum2 += *(a + i);
 		i += size - 1;
 	}
 	printf("%d\n", sum2);
