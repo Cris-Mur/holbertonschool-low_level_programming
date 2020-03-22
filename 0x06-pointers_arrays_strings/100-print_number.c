63b85bcea830d5db2c46de70b8ad0211c150d487
Cris-Mur <yomy98@gmail.com>
2020-03-22 10:53:53 -0500

    [after long time]

--- 0x06-pointers_arrays_strings/100-print_number.c
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -1,12 +1,40 @@
 #include "holberton.h"
-
+#include <stdio.h>
+#include <limits.h>
 /**
- * print_number - print number
+ * print_number - print n number
  * @n: input integer
  * Return: void
  */
 
 void print_number(int n)
 {
-	n++;
+	int mod = 10;
+
+	if (n < 0)
+	{
+		_putchar(45);
+	}
+	if (!n)
+	{
+		_putchar(48);
+		return;
+	}
+	if (n < 0)
+	{
+		n *= -1;
+	}
+	if (n < 10 && n > 0)
+	{
+		_putchar((n) + 48);
+		return;
+	}
+	while (n < 0 ? (((unsigned int)n / mod) > 9) : (n / mod > 9))
+		mod = mod * 10;
+	while (mod >= 1)
+	{
+		_putchar(n < 0 ? (((n / mod) * -1) + 48) : ((n / mod) + 48));
+		n = n % mod;
+		mod = mod / 10;
+	}
 }
