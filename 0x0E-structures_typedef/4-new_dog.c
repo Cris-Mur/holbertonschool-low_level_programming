d4db716f82a6f87e5beea18d72fa1ebb12390ace
Cris-Mur <yomy98@gmail.com>
2020-04-13 12:38:55 -0500

    [hot fix]make my strdup

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,6 +2,26 @@
 #include <string.h>
 
 /**
+ * _strdup - duplicate a str in heap memory
+ * @str: input string
+ * Return: duplicated string
+ */
+char *_strdup(char *str)
+{
+	size_t sz = 0, x;
+	char *dup = NULL;
+	while (str[sz])
+		sz++;
+	dup = malloc(sz * (sizeof(char)));
+	if (!dup)
+		return (NULL);
+	for (x = 0; x < sz; x++)
+		dup[x] = str[x];
+	return (dup);
+}
+
+
+/**
  * new_dog - funtion that create
  * @name: string of structure
  * @age: float of the structure
@@ -17,8 +37,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (!doge)
 		return (NULL);
 
-	doge->name = strdup(name);
-	doge->owner = strdup(owner);
+	doge->name = _strdup(name);
+	doge->owner = _strdup(owner);
 	doge->age = age;
 	return (doge);
 
