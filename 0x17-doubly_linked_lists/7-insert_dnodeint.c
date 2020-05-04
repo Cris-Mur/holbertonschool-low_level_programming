34427ed4ab8559b82353d7a99aead101baaee702
Cris-Mur <yomy98@gmail.com>
2020-05-03 20:39:59 -0500

    [only fix betty]

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,6 +1,23 @@
 #include "lists.h"
 
 /**
+ * put_mid - put the new node in the middle
+ * @nxt: [NEW]->[NXT]
+ * @new: <-[NEW]->
+ * @prv: [PRV]->[NEW]
+ * Return: new node address
+ */
+
+dlistint_t *put_mid(dlistint_t *nxt,  dlistint_t *new, dlistint_t *prv)
+{
+	new->next = nxt;
+	prv->next = new;
+	nxt->prev = new;
+	new->prev = prv;
+	return (new);
+}
+
+/**
  * insert_dnodeint_at_index - insert in the middle of the list
  * @h: input doubly linked list
  * @idx: number of iterations in the list
@@ -10,7 +27,7 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	unsigned int gansito;
-	dlistint_t *new, *temp = NULL, *teemoNXT = NULL;
+	dlistint_t *new, *temp = NULL;
 
 	if (!h)
 		return (NULL);
@@ -28,24 +45,22 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			temp = temp->next;
 			if (!temp)
 			{
-				free(new), return (NULL);
+				free(new);
+				return (NULL);
 			}
 			gansito++;
 		}
 		if (temp->next)
-		{
-			teemoNXT = temp->next, new->next = teemoNXT;
-			temp->next = new, teemoNXT->prev = new;
-			new->prev = temp;
-		}
+			return (put_mid(temp->next, new, temp));
 		else
 		{
-			free(new), return (add_dnodeint_end(h, n));
+			free(new);
+			return (add_dnodeint_end(h, n));
 		}
 	}
 	else
 	{
-		free(new), return (add_dnodeint(h, n));
+		free(new);
+		return (add_dnodeint(h, n));
 	}
-	return (new);
 }
