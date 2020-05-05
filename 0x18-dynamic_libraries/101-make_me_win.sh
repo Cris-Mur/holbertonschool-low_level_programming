a974390d5f5b17f0de03f5fe3cee1e483254306a
Cris-Mur <yomy98@gmail.com>
2020-05-04 23:09:18 -0500

    [jajajja que errorcito]

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /temp/ https://raw.github.com/Cris-Mur/holbertonschool-low_level_programming/master/0x18-dynamic_libraries/coso.so
+wget -P /tmp/ https://raw.github.com/Cris-Mur/holbertonschool-low_level_programming/master/0x18-dynamic_libraries/coso.so
 export LD_PRELOAD=/tmp/coso.so
