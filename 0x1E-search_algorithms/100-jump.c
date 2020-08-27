c34a6484b6affc639653475829e4e08e88c2decd
Cris-Mur <yomy98@gmail.com>
2020-08-27 14:33:43 -0500

    [out index]

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -16,11 +16,14 @@ int jump_search(int *array, size_t size, int value)
 
 	start = step = 0;
 
-	while (step < size)
+	while (step <= size)
 	{
-		if (array[step] >= value)
+		if (array[step] >= value || step == size - 1)
 		{
-			printf("Value found between indexes [%lu] and [%lu]\n", start, step);
+			if (step == size - 1)
+				printf("Value found between indexes [%lu] and [%lu]\n", start, size);
+			else
+				printf("Value found between indexes [%lu] and [%lu]\n", start, step);
 			for (; start <= step; start++)
 			{
 				printf("Value checked array[%lu] = [%d]\n", start, array[start]);
@@ -34,6 +37,8 @@ int jump_search(int *array, size_t size, int value)
 		printf("Value checked array[%lu] = [%d]\n", step, array[step]);
 		start = step;
 		step = step + sqrt(size);
+		if (step >= size)
+			step = size - 1;
 	}
 	printf("Value found between indexes [%lu] and [%lu]\n", start, step);
 	printf("Value checked array[%lu] = [%d]\n", start, array[start]);
