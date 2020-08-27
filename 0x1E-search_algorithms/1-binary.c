74a4353790863efa31d783f45a4c973b537de8f7
Cris-Mur <yomy98@gmail.com>
2020-08-27 12:18:08 -0500

    [print limits]

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -9,7 +9,7 @@ void pr_arr(int *arr, size_t n)
 {
 	size_t x;
 
-	if (!arr)
+	if (!arr || n <= 0)
 		return;
 
 	x = 0;
@@ -38,7 +38,7 @@ int binary_search(int *array, size_t size, int value)
 	l = 0;
 	r = size - 1;
 	pr_arr(array, size);
-	while (l < r)
+	while (l <= r)
 	{
 		mid = (l + r) / 2;
 		if (array[mid] == value)
